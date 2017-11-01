c0604676a611b8b4cf5cef7bd30c5d2dfe15ca17
fernandom06 <fernandom06@hotmail.com>
2017-11-01 19:46:03 +0100

    Apartado 2.2

--- src/Juego.java
+++ src/Juego.java
@@ -1,8 +1,11 @@
 public class Juego {
-    public int vidas;
+    private int vidas;
+    private int vidasInicial;
+    private int record=0;
 
     public Juego(int vidasIniciales){
         vidas=vidasIniciales;
+        vidasInicial=vidasIniciales;
     }
     /*public void muestraVidasRestantes(){
         System.out.println("Te quedan "+vidas+" vidas");
@@ -12,7 +15,27 @@ public class Juego {
          return vidas;
     }
 
+    public int quitaVida(){
+        vidas--;
+        if(vidas<1){
+            System.out.println("Juego Terminado");
+        }
+        return vidas;
+    }
 
+    public void reiniciaPartida(){
+        vidas=vidasInicial;
+    }
+
+    public void actualizaRecord(){
+        if (vidas==record){
+            System.out.println("Has alcanzado el record");
+        }
+        if (vidas>record){
+            record=vidas;
+            System.out.println("Se ha batido el record, el nuevo valor es "+record);
+        }
+    }
 
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
-
+    /*lo de la clase aplicacion no tiene mucho sentido*/
     public static void main(String[] args) {
         //System.out.println("Hello World!");
-        Juego juego1=new Juego(4);
+        Juego juego1=new Juego(1);
         juego1.muestraVidasRestantes();
-        juego1.vidas--;
+        juego1.quitaVida();
+        juego1.reiniciaPartida();
         juego1.muestraVidasRestantes();
+        juego1.actualizaRecord();
         Juego juego2=new Juego(5);
-        juego1.muestraVidasRestantes();
-        juego2.muestraVidasRestantes();
     }
 }
