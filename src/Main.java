c9aa0aad271fd9c111ff38c48d997ed4c0628fe7
fernandom06 <fernandom06@hotmail.com>
2017-11-01 19:24:48 +0100

    Realizado el apartado 2.1 de la primera practica

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //System.out.println("Hello World!");
+        Juego juego1=new Juego(4);
+        juego1.muestraVidasRestantes();
+        juego1.vidas--;
+        juego1.muestraVidasRestantes();
+        Juego juego2=new Juego(5);
+        juego1.muestraVidasRestantes();
+        juego2.muestraVidasRestantes();
     }
 }
